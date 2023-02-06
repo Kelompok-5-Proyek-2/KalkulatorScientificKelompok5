a953999888a9b0c929636a112d07729e6739a6d0
barrybraun <barry158cyber@gmail.com>
2023-02-06 11:42:58 +0700

    <add> Nambahin file barry.h

---
+++ barry.h
@@ -0,0 +1,31 @@
+#ifndef statistika_h
+#define statistika_h
+#include <math.h>
+#include <stdio.h>
+
+float HitungMean(int BanyakData, float mean[])
+{
+	int x;
+	float sum = 0.0;
+	for (x = 0; x < BanyakData; ++x)
+	{
+		sum += mean[x];
+	}
+	return sum / BanyakData;
+}
+
+void input_Statistika_Mean()
+{
+	int BanyakData, x;
+  	float mean[25];
+  	printf("Jumlah data yang akan dimasukkan :  ");
+  	scanf("%d", &BanyakData);
+  	for (x = 0; x < BanyakData; ++x)
+	{
+    	printf("Masukkan angka : ");
+    	scanf("%f", &mean[x]);
+	}
+  	printf("Nilai Mean = %.2f", HitungMean(BanyakData, mean));
+}
+
+#endif
