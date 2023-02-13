332f6d8ce322d093173f7c15b102320ba5cc985b
Muhammad Syaifullah <mahkotonagata@gmail.com>
2023-02-13 11:01:49 +0700

    <add> menambahkan modul mencari desimal dan boolean

--- syaiful.h
+++ syaiful.h
@@ -1,9 +1,12 @@
 #ifndef syaiful_h
 #include <math.h>
+#define true 1
+#define false 0
 
-float Hitungpersen(float persen, float total){
+float Hitungpersen(float persen, float total){// mencari persentase
 float hasil;
 hasil = (persen/total)*100;
+
 return hasil;
 }
 
@@ -15,10 +18,67 @@ float Input_htgprsn(){
 	printf("Dari jumlah total : ");
 	scanf("%f", &b);
 	hsl=Hitungpersen(a,b);
-	printf("Adalah : %0.lf Persen", hsl);  
+	printf("Adalah : %2.lf Persen\n\n", hsl); 
+
 }
 
+float Hitungdesimal(float pembilang, float penyebut){//mencari desimal
+	double hsldsml;
+	hsldsml=(double)pembilang/penyebut;
+	
+	return hsldsml;
+}
+
+float Input_desimal(){
+	float a,b;
+float hdsml;
+	printf("Pembilang : ");
+	scanf("%f", &a);
+	printf("Penyebut : ");
+	scanf("%f", &b);
+	hdsml=Hitungdesimal(a,b);
+	printf("Hasil desimal :%.2f ",hdsml);
+}
+
+int boln(int angkaA,int angkaB){// mencari boolean
+	char hslbln;
+	if (angkaA>angkaB){
+		printf("%i > %i : benar\n",&angkaA, &angkaB);
+	}else{
+		printf(">  : salah\n");
+	}
+	if (angkaA==angkaB){
+		printf(">= : benar\n");
+	}else{
+		printf(">= : salah\n");
+	}
+	if (angkaA<angkaB){
+		printf("<  : benar\n");
+	}else{
+		printf("<  : salah\n");
+	}if (angkaA==angkaB){
+		printf("<= : benar\n");
+	}else{
+		printf("<= : salah\n");
+	}if (angkaA==angkaB){
+		printf("=  : benar\n");
+	}else{
+		printf("=  : salah\n");
+	}
+}
+
+void tampilbln(){
+	int a,b;
+	int hasilbln;
+	printf("\n\nangka pertama :");
+	scanf("%d", &a);
+	printf("angka kedua :");
+	scanf("%d", &b);
+	hasilbln=boln(a,b);
+}
+ 
 #endif
+
 
 
 
